fbf10145aad89b0b291f02560edb26a39e1e9409
Holger17 <Holger.noormets@gmail.com>
2023-10-16 17:42:50 +0300

    commit

---
+++ JS_harjutused/main.js
@@ -0,0 +1,18 @@
+function harjutus1(nimi) {
+    console.log("tere tulemast " + nimi);
+}
+
+harjutus1("Holger")
+
+function harjutus2(mark, mudel, varv){
+
+    const name = "Valitud auto on ";
+    console.log("Valitud auto on " + varv + " " + mark + " " + mudel + "." );
+}
+
+harjutus2("Ford", "Mustang" ,"Sinine")
+
+
+function harjutus3 (Pindala, korgus){
+    
+}
